5cebffc9295e4de13f28a040a2830cabfe95c9d9
wanmin <wanminny@163.com>
2017-06-07 15:05:16 +0800

    add schedule

---
+++ src/main/java/com/example/demo/schedule/sche.java
@@ -0,0 +1,25 @@
+package com.example.demo.schedule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Created by wanmin on 2017/6/7.
+ */
+
+@Configuration
+@EnableScheduling
+public class sche {
+
+    @Scheduled(cron = "*/10 * * * * ?")
+    public void schedule(){
+        System.out.println("dida!");
+    }
+
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void schedule11(){
+        System.out.println("schedule11!");
+    }
+
+}
